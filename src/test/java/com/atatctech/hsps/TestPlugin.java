d9cd8cd8aff6b1cca596281e4cfa5ed6417682c5
ATATC <futerry@outlook.com>
2023-05-21 15:18:02 -0400

    Test.

--- src/test/java/com/atatctech/hsps/TestPlugin.java
+++ src/test/java/com/atatctech/hsps/TestPlugin.java
@@ -3,7 +3,7 @@ package com.atatctech.hsps;
 import com.atatctech.hsps.verification.Signed;
 import org.jetbrains.annotations.Nullable;
 
-@Signed("RC7xd6NuB8TkWybbZwlrVzczmfvNPAA0v1VjH/XomW8kwQWuojEUkqBc1GwZmMDl56bfoARut1xjcscaF97trirvYYvi8R/jJginyybElDEHnBktMJ42FY05j+4bxaLkSwUpekgXv13GybiEq9E7IOFlM9I+kS/RDsMYWHR7sSIyOZlLGwSG68qtfZnaynicqYu/QGCzXVGTf7uLQlmM3CSlTfYYf/CMCYAZKzBBFDk2rBUVw4uG1LzN6SI9ukbvzxwaZ/tlmWgz/zPrhPu4T2pmuNPohjPbUKmft5aesg6G36/ptD8JA0+m6G150aoZ5uT00HbMdDg0u+R2ibKpzg==")
+@Signed("KmmLlCogjpsVVRdbkchtiZI4DlINUDZWJG3bIgyWXZuczEZ9zh+zP6zZI+WcZ5LvqjajVUVgxB/zp39yeQ+eKrBvja1c13Bupns9UvrJAQUVR3Uam8uHOnvhVIdVeNk7uKxOYJyDh8PE2K/0uag4HymgMikBQ5jy2wqAV8FItH/XZdjqRFANQ2G/U6fDDspjrIbeifr+nML5me/R+0nUY5RHdd1NTiGXjgEvLFj3OIDiO90FNhuIy7wV9NxA9TyNcFB94aANYiqe4oyYUD5rgpPV+Xzqs+gWTAO4h/W0YSOYYYeABYtDpOtv+N+iCC2OP4hqHSZowxczGzi4QAN9TA==")
 public class TestPlugin implements Plugin {
     public static void run() {
         System.out.println("test");
